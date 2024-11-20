9950daae2ea6c8bd61198243e6eaa2c4671e95c3
cguillo <clement.guillo@insee.fr>
2024-11-20 19:48:24 +0000

    etat des recherches

--- commandes.sh
+++ commandes.sh
@@ -3,8 +3,10 @@
  cd content/models
  
  # -> le ckpt :) -> monte un volume pour ça ?
- https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16
- curl https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16
- ou wget et renommer le fichier
+ wget https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16
+ 
+ J'ai tené de renommer le fichier au pire je l'injecterai dans minion à partir de mon pc .
 
  kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-hdhdk:/content/models/model.safetensors
+
+ le kubectl ne march epas bien pour des problemes de droit semble t'il que j'ai essayé de contourner avec une spec dans le deployment
