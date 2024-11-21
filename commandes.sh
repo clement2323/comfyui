ea5f9e1fdc1fd902a416d68cf9573554786ccc3f
cguillo <clement.guillo@insee.fr>
2024-11-21 12:59:53 +0000

    repertoire de placement des models trouvé

--- commandes.sh
+++ commandes.sh
@@ -22,3 +22,7 @@ kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-qsfh6:/content/model
 kubectl cp comfyui-deployment-f564fd44d-qsfh6:/content/models/model.safetensors ./model.safetensors -o tar | pv > output
 kubectl exec comfyui-deployment-f564fd44d-qsfh6 -- cat /content/models/model.safetensors | pv 
 # RAJOUTER le GIT CLONE etc.. pour comfy manager !
+
+kubectl exec -it comfyui-deployment-6fb74675cf-67npm -- bash
+mkdir -p /app/output/checkpoints
+kubectl cp model.safetensors comfyui-deployment-6fb74675cf-67npm:/app/output/checkpoints
