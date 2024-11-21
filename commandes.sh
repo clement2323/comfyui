88aebc1a2704004765c6555c04d88daf7f379415
cguillo <clement.guillo@insee.fr>
2024-11-21 11:11:30 +0000

    beau deploielment avec pre copie de model

--- commandes.sh
+++ commandes.sh
@@ -7,7 +7,18 @@
  
  J'ai tenté de renommer le fichier au pire je l'injecterai dans minion à partir de mon pc .
 
- kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-hdhdk:/content/models/model.safetensors
+ kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-qsfh6:/content/models/checkpoints/model.safetensors
 
  #le kubectl ne marchait  pas bien pour des problemes de droit semble t'il que j'ai essayé de contourner avec une spec dans le deployment là ça marche
  le kubectl marche !! ne manqyue plus qu'yun vrai téléchargement de modele ?
+# rajouter le manager
+
+kubectl cp <namespace>/<pod>:/path/to/remote/file - | pv > output
+
+
+sudo apt-get update
+sudo apt-get install pv
+kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-qsfh6:/content/models/model.safetensors - | pv > output
+kubectl cp comfyui-deployment-f564fd44d-qsfh6:/content/models/model.safetensors ./model.safetensors -o tar | pv > output
+kubectl exec comfyui-deployment-f564fd44d-qsfh6 -- cat /content/models/model.safetensors | pv 
+# RAJOUTER le GIT CLONE etc.. pour comfy manager !
