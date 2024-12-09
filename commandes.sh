4a7eee0899c5514a62d94360015265f8e3f7a42c
cguillo <clement.guillo@insee.fr>
2024-12-09 19:06:28 +0000

    modify deployment with manger and Ipadapter

--- commandes.sh
+++ commandes.sh
@@ -6,9 +6,8 @@
  wget https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16
  
  J'ai tenté de renommer le fichier au pire je l'injecterai dans minion à partir de mon pc .
-
  kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-qsfh6:/content/models/checkpoints/model.safetensors
-
+ 
  #le kubectl ne marchait  pas bien pour des problemes de droit semble t'il que j'ai essayé de contourner avec une spec dans le deployment là ça marche
  le kubectl marche !! ne manqyue plus qu'yun vrai téléchargement de modele ?
 # rajouter le manager
