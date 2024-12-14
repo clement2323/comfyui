8bf5ac0bcc25a8dfaaf005fb5bff95dad135f250
cguillo <clement.guillo@insee.fr>
2024-12-14 12:48:38 +0000

    ajout de commandes pour installer insight face et utiliser ipadapter

--- commandes.sh
+++ commandes.sh
@@ -1,16 +1,18 @@
+ 
+ install insoght face onngx etc..
+command:
+        - /bin/sh
+        - -c
+        - |
+          apt update
+          apt install -y g++
+          pip install insightface
+          pip install onnxruntime
+          pip install onnxruntime-gpu
+
+
  kubectl exec -it comfyui-deployment-f564fd44d-hdhdk -- /bin/bash
- cd ../
- cd content/models
  
- # -> le ckpt :) -> monte un volume pour ça ?
- wget https://civitai.com/api/download/models/66991?type=Model&format=SafeTensor&size=full&fp=fp16
- 
- J'ai tenté de renommer le fichier au pire je l'injecterai dans minion à partir de mon pc .
- kubectl cp ./model.safetensors comfyui-deployment-f564fd44d-qsfh6:/content/models/checkpoints/model.safetensors
- 
- #le kubectl ne marchait  pas bien pour des problemes de droit semble t'il que j'ai essayé de contourner avec une spec dans le deployment là ça marche
- le kubectl marche !! ne manqyue plus qu'yun vrai téléchargement de modele ?
-# rajouter le manager
 
 kubectl cp <namespace>/<pod>:/path/to/remote/file - | pv > output
 
